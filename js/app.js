b9115e56fdd8f693e965803c0867afbbff3b8a25
CARLOS SUAREZ <carlos@MAC-IMAC-CSUAREZ.local>
2017-09-14 08:52:35 -0500

    home_loginButtonOk_v1

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	$("[data-openDropdown]").hover(function() {
+	$("[data-openDropdown]").click(function() {
 		var theDropdown = $(this).attr('data-openDropdown')
-		var dropdown = $(theDropdown)
-		dropdown.addClass('active');
-	});
-
-	$("[data-openDropdown]").mouseleave(function() {
-		var theDropdown = $(this).attr('data-openDropdown')
-		var dropdown = $(theDropdown)
-		dropdown.removeClass('active');
-	});
-	
-	$("[data-closeDropdown]").mouseleave(function() {
-		var theDropdown = $(this).attr('data-closeDropdown')
 		var dropdown = $(theDropdown)
 		dropdown.toggleClass('active');
 	});
+
+	// $("[data-openDropdown]").click(function() {
+	// 	var theDropdown = $(this).attr('data-openDropdown')
+	// 	var dropdown = $(theDropdown)
+	// 	dropdown.removeClass('active');
+	// });
+	
+	// $("[data-closeDropdown]").mouseleave(function() {
+	// 	var theDropdown = $(this).attr('data-closeDropdown')
+	// 	var dropdown = $(theDropdown)
+	// 	dropdown.toggleClass('active');
+	// });
 });
