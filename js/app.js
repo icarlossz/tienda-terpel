1e19ec45d16bc73dee9fd2ebbce35a85b5563719
CARLOS SUAREZ <carlos@MAC-IMAC-CSUAREZ.local>
2017-09-15 09:48:52 -0500

    home_catalogoOk_v1

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function(){
 	$("[data-openDropdown]").click(function() {
 		var theDropdown = $(this).attr('data-openDropdown')
 		var dropdown = $(theDropdown)
+		var allDropdowns = $('.Dropdown')
+
 		dropdown.toggleClass('active');
+
+		allDropdowns.each(function(i, e) {
+
+			if (dropdown[0] !== e) {
+				$(e).removeClass('active')
+			}
+		})
+
 	});
 
-	// $("[data-openDropdown]").click(function() {
-	// 	var theDropdown = $(this).attr('data-openDropdown')
-	// 	var dropdown = $(theDropdown)
-	// 	dropdown.removeClass('active');
+	// $(document).ready(function(){
+	//     $(".modal").fadeIn();
+	//     $(".cerrar").click(function(){
+	//         $(".modal").fadeOut(300);
+	//     });
 	// });
-	
-	// $("[data-closeDropdown]").mouseleave(function() {
-	// 	var theDropdown = $(this).attr('data-closeDropdown')
-	// 	var dropdown = $(theDropdown)
-	// 	dropdown.toggleClass('active');
-	// });
+	 
 });
