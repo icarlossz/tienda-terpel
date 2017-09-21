cb60710acfe1db896484ad4a6049c3e577679e50
Carlos Suarez <me@carlossz.co>
2017-09-21 09:19:35 -0500

    homeok_checkoutok_favoritesok_encuestaok_faqok

--- js/app.js
+++ js/app.js
@@ -33,6 +33,22 @@ $(document).ready(function(){
 
 
 
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 500);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(500);
+		} else {
+			$('.ir-arriba').slideUp(500);
+		}
+	});
+
+
+
 	// $(document).ready(function(){
 	//     $(".modal").fadeIn();
 	//     $(".cerrar").click(function(){
