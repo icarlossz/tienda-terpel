1518865a50b27e7cb2d27c57e0e9796acba72898
Carlos Suarez <me@carlossz.co>
2017-09-25 08:54:22 -0500

    homeok_fixedminorbugs_tabsworking

--- js/app.js
+++ js/app.js
@@ -7,31 +7,128 @@ $(document).ready(function(){
 		dropdown.toggleClass('active');
 
 		allDropdowns.each(function(i, e) {
-
-			if (dropdown[0] !== e) {
-				$(e).removeClass('active')
-			}
+			dropdown[0] !== e && $(e).removeClass('active')
 		})
-
+		$(".invisible-closer").click(function(){ dropdown.removeClass('active') })
 	});
 
 	$("[data-openTab]").click(function() {
+		// Add active class to the tab
+		var tabControler = $('.tab').removeClass('active')
+		$(this).addClass('active')
+
+		// Add active class to the content
 		var theTab = $(this).attr('data-openTab')
 		var tab = $(theTab)
 		var allTabs = $('.Tabs')
 
-		tab.toggleClass('active');
-
-		allTabs.each(function(i, e) {
-
-			if (tab[0] !== e) {
-				$(e).removeClass('active')
-			}
+		tab.removeClass('active');
+		allTabs.each(function(index, element) {
+			tab[0] !== element && $(element).removeClass('active')
 		})
-
+		tab.addClass('active');
 	});
 
+	$("[data-openModalTab]").click(function() {
+		// Add active class to the tab
+		var tabs = $('.modal-tab-title')
+		tabs.removeClass('active')
+		$(this).addClass('active')
 
+		// Add active class to the content
+		var theModalTab = $(this).attr('data-openModalTab')
+		var modalTab = $(theModalTab)
+		var modalTabs = $('.modal-tab')
+
+		modalTab.removeClass('active');
+		modalTabs.each(function(index, element) {
+			modalTab[0] !== element && $(element).removeClass('active')
+		})
+		modalTab.addClass('active');
+	});
+
+	$("[data-faqTab]").click(function() {
+		// Add active class to the tab
+		var tabs = $('.faq-tab-controller')
+		tabs.removeClass('active')
+		$(this).addClass('active')
+
+		// Add active class to the content
+		var theFaqTab = $(this).attr('data-faqTab')
+		var faqTab = $(theFaqTab)
+		var faqTabs = $('.faq-tab')
+
+		faqTab.removeClass('active');
+		faqTabs.each(function(index, element) {
+			faqTab[0] !== element && $(element).removeClass('active')
+		})
+		faqTab.addClass('active');
+	});
+
+	$("[data-itemDetalle]").click(function() {
+		// Add active class to the tab
+		$(this).fadeIn('slow')
+		var buttons = $('[data-itemDetalle]')
+		buttons.each(function(index, element){
+			if (this == element) {
+				$(this).attr('src') == 'images/iconMore.svg'
+					? $(this).attr('src', 'images/closeBurger.svg')
+					: $(this).attr('src', 'images/iconMore.svg')
+			}
+		})
+		
+
+		// Add active class to the content
+		var theItemDetalle = $(this).attr('data-itemDetalle')
+		var itemDetalle = $(theItemDetalle)
+		var itemsDetalle = $('.item-detalle')
+
+		itemDetalle.toggleClass('active');
+		itemsDetalle.each(function(index, element) {
+			itemDetalle[0] !== element && $(element).removeClass('active')
+		})
+		// itemDetalle.addClass('active');
+	});
+
+	$("[data-emotional]").click(function() {
+		// Add active class to the tab
+		var emotions = $('.emotionp')
+		emotions.removeClass('is-active')
+		$(this).addClass('is-active')
+
+		// Add active class to the content
+		var theEmotion = $(this).attr('data-emotional')
+		var emotion = $(theEmotion)
+		var emotions = $('.emotionsContainer')
+
+		if (theEmotion == '#emotionsFormGood') {
+			$('#emotionsFormBad').removeClass('active')
+			$('#emotionsFormGood').addClass('active')
+			console.log($('#emotionsFormBad'), $('#emotionsFormgood'))
+		} else {
+			$('#emotionsFormGood').removeClass('active')
+			$('#emotionsFormBad').addClass('active')
+			console.log(theEmotion)
+		}
+		// emotion.addClass('active');
+	});
+
+  $("[data-outProduct]").click(function() {
+  	var productID = $(this).attr('data-outProduct');
+  	$(productID).addClass('invisible')
+  });
+
+  $("[data-toggleActive]").click(function() {
+  	var commonElementsAttribbute = $(this).attr('data-toggleActive');
+  	var elements = $(commonElementsAttribbute);
+
+  	$(elements).each(function(index, element){
+  		$(element).removeClass('active')
+  		$(element).next().removeClass('active')
+  	})
+  	$(this).addClass('active')
+  	$(this).next().addClass('active')
+  });
 
 	$('.ir-arriba').click(function(){
 		$('body, html').animate({
@@ -47,16 +144,11 @@ $(document).ready(function(){
 		}
 	});
 
- 
-
-
-
 	// $(document).ready(function(){
 	//     $(".modal").fadeIn();
 	//     $(".cerrar").click(function(){
 	//         $(".modal").fadeOut(300);
 	//     });
 	// });
-	 
 });
 
